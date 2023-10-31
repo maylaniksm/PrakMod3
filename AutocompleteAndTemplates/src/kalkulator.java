c860d646faecbf28862f34f4a68ec0b7ad1b71fa
maylaniksm <maylaniwardhani@gmail.com>
2023-10-31 11:16:27 +0700

    first commit

---
+++ AutocompleteAndTemplates/src/kalkulator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+public class kalkulator {
+
+    public static void main(String args[]) {
+        //penambahan
+        int a = 1 + 2 ;
+        out.println("Hasil penambahan: " + a);
+
+//        int $var1$ = $value1$ * $value2$;
+//        System.out.println("Hasil perkalian: " + $var1$);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
